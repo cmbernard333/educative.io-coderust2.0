e6b96b33bb44462b6ebfc89dfed0beaf0460c75d
Christian Bernard <cmbernard333@gmail.com>
2017-08-11 13:02:54 -0400

    rotateArray now works for positive and negative rotations. swap was updated to work properly when it isn't using 0 and the length as the bounds.

--- Arrays/RotateArray/java/RotateArray.java
+++ Arrays/RotateArray/java/RotateArray.java
@@ -19,35 +19,41 @@ public class RotateArray {
 
     public <T> void reverse(T [] arr, int start, int end)
     {
-        System.out.println("--- Reverse arr="+Arrays.toString(arr)+" from "+start+" to "+end+" ----");
-        int size = end; // you don't use absolute size because we are counting from the back
-        for(int i = end; i > ((size)/2); i--)
+        for(int i = end; i > ((end)/2); i--)
         {
-            // System.out.println(String.format("Swapping arr[%d]=%d, arr[%d]=[%d]",i, arr[i], (size-i), arr[size-i]));
-            swap(arr, i, ((end)-i)+start); // have to add start in case start isn't 0 - we don't want negative indices
+            if(i > ((end-i)+start)) // this fixes the issue of swapping the middle twice
+            {
+                swap(arr, i, (end-i)+start); // calculate for negative indices
+            }
         }
-        System.out.println("--- Reverse ----");
     }
 
     public <T> void rotateArray(T [] arr, int rotate)
     {
+        rotate = rotate % arr.length;
+        if(rotate<0)
+        {
+            rotate = rotate + arr.length;
+        }
         this.reverse(arr,0,arr.length-1); // reverse the whole thing
-        System.out.println(Arrays.toString(arr));
         this.reverse(arr,0,rotate-1); // reverse the elements included in the rotation
-        System.out.println(Arrays.toString(arr));
         this.reverse(arr,rotate,arr.length-1); // reverse the upper half back to original order
-        return;
     }
 
     public static void main(String [] args)
     {
         /* this has to be the Integer wrapper type because you can't convert from primitives to objects */
         Integer hello[] = {1,10,20,0,59,86,32,11,9,40};
+        Integer trivial[] = {1,2,3};
         RotateArray r = new RotateArray();
+        // positive rotation
         System.out.println(Arrays.toString(hello));
-        // r.reverse(hello,0,hello.length-1);
         r.rotateArray(hello, 2);
         System.out.println(Arrays.toString(hello));
+        // negative rotation
+        System.out.println(Arrays.toString(trivial));   
+        r.rotateArray(trivial, -1);
+        System.out.println(Arrays.toString(trivial));
         
     }
 }
