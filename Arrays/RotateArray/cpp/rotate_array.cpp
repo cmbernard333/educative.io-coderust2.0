d3c555d7cdb66614314043b59ceb341b661f346d
Christian Bernard <cmbernard333@gmail.com>
2017-08-11 13:40:20 -0400

    completing the rotate array code for c++

--- Arrays/RotateArray/cpp/rotate_array.cpp
+++ Arrays/RotateArray/cpp/rotate_array.cpp
@@ -15,16 +15,32 @@ void swap(T* x, T* y)
  * @return none
  */
 template<typename T>
-void reverse(T* arr, std::size_t size)
+void reverse(T* arr, std::size_t size, int start, int end)
 {
     /* size = 3; stop at 1 */
     /* size = 4; stop at 2 */
-    for(std::size_t i = size-1; i > (size/2)-1; i--)
+    T* pstart = &arr[start];
+    T* pend = &arr[end];
+    while(pstart < pend)
     {
-       /* 1 2 3 -> 3 2 1*/
-       /* 8 7 4 2 -> 2 4 7 8 */ 
-        swap(&arr[i], &arr[(size-1)-i]);
-    } 
+        swap(pstart,pend);
+        pstart++;
+        pend--;
+    }
+}
+
+template<typename T>
+void rotate_array(T* arr, std::size_t size, int rotate)
+{
+    rotate = rotate % size;
+    if(rotate < 0)
+    {
+        rotate = rotate + size;
+    }
+    reverse(arr, size, 0, size-1);
+    reverse(arr, size, 0, rotate-1);
+    reverse(arr, size, rotate,size-1);
+    return;
 }
 
 template<typename T>
@@ -41,7 +57,8 @@ int main(int argc, char** argv)
 {
     int hello[10] = {1,10,20,0,59,86,32,11,9,40};
     print_arr(hello,10);
-    reverse(hello,10);
+    rotate_array(hello,10,2);
+    // reverse(hello,10,0,9);
     print_arr(hello,10);
     return 0;
 }
