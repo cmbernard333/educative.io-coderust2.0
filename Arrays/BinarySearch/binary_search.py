9a003cb653d34d8260ddbdbec65efa6d74885cf8
Christian Bernard <cmbernard333@gmail.com>
2017-08-11 15:35:04 -0400

    fixed an issue with the iterative binary search where edges of the array weren't being considered properly.

--- Arrays/BinarySearch/binary_search.py
+++ Arrays/BinarySearch/binary_search.py
@@ -11,19 +11,19 @@ def binary_search(arr,ele,l,h):
             return binary_search(arr,ele,mid+1,h) # mid + 1 because you already considered it
 
 def binary_search_iter(arr, ele, l ,h):
-    while l < h:
+    while l <= h:
         mid = int((l + h) /2)
         if ele == arr [mid]:
             return mid
         elif ele > arr[mid]:
             l = mid+1
         elif ele < arr[mid]:
-            h = mid
+            h = mid-1
     return -1
 
 if __name__ == '__main__':
     arr = [2,5,8,9,11,13]
-    find = [1,2,3,4,5]
+    find = [1,2,3,4,5,13]
     print(arr)
     print('Recursive')
     for value in find: 
