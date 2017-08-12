4af878f6d5a346fa39c2e5c5b7ab408cfc002c8d
Christian Bernard <cmbernard333@gmail.com>
2017-08-12 09:08:09 -0400

    Added debug logging when using the -DDebug=true flag. fixed reverse_linked_list so it actually works. its actually simpler now.

--- Arrays/ReverseSingleyLinkedList/LinkedListNode.java
+++ Arrays/ReverseSingleyLinkedList/LinkedListNode.java
@@ -1,5 +1,7 @@
 class LinkedListNode<E> {
 
+    public static boolean DEBUG_MODE = false;
+
     private LinkedListNode<E> next;
     private E data;
 
@@ -36,18 +38,24 @@ class LinkedListNode<E> {
         null<-0<-1<-2<-3<-4<-5
     */
     public static <E> LinkedListNode<E> reverse(LinkedListNode<E> head) {
-        LinkedListNode<E> cur = head;
-        LinkedListNode<E> prev = head, next = null;
+        LinkedListNode<E> cur = head, prev = null, next = head;
         /* prev starts at head */
-        while(cur != null)
+        while((next = cur.getNext()) != null)
         {
+            System.out.println("Node: data("+cur.getData()+"), next->"+cur.getNext().getData());
+            /*
+            prev = cur;
             cur = cur.getNext(); // grab the next pointer
             next = cur.getNext(); // grab the next-next pointer
             cur.setNext(prev); // set next pointer to previous pointer
+            cur = next;
+            */
+            cur.setNext(prev);
             prev = cur;
             cur = next;
         }
-        head.setNext(null);
+        cur.setNext(prev);
+        System.out.println("Reverse finished");
         return cur;
     }
 
@@ -55,7 +63,18 @@ class LinkedListNode<E> {
     public String toString() {
         StringBuilder builder = new StringBuilder();
         LinkedListNode<E> cur = this;
+        if(DEBUG_MODE)
+        {
+            System.out.println("Printing list");
+        }
         while(cur != null) {
+            if(DEBUG_MODE)
+            {
+                System.out.println(cur.getData().toString() + ((cur.getNext() != null) ? "->" : ""));
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) { /* don't care */}
+            }
             builder.append(cur.getData().toString() + ((cur.getNext() != null) ? "->" : ""));
             cur = cur.getNext();
         }
@@ -65,6 +84,11 @@ class LinkedListNode<E> {
     public static void main(String [] args) {
         LinkedListNode<Integer> head = new LinkedListNode<Integer>(0);
         LinkedListNode<Integer> tmp = head;
+        LinkedListNode.DEBUG_MODE = false;
+        if(System.getProperty("Debug") != null)
+        {
+            LinkedListNode.DEBUG_MODE = System.getProperty("Debug").equalsIgnoreCase("true");
+        }
         for(int i = 1;i < 6;i++) {
             tmp.setNext(new LinkedListNode<Integer>(i));
             tmp = tmp.getNext();
